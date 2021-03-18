c9bd0d8ee431a36f1bb2dab6d1533f8ef8f613be
Katrin Nitsche <katrin.nitsche@ecomsilver.com>
2021-03-18 06:54:42 +0000

    wip

--- src/MyHelpersApp.API/MyHelpersApp.API/obj/Release/net5.0/MyHelpersApp.API.AssemblyInfo.cs
+++ src/MyHelpersApp.API/MyHelpersApp.API/obj/Release/net5.0/MyHelpersApp.API.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("735daa84-4972-4ecb-80a6-a2927b68b6ce")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHelpersApp.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
